951a09a67eacc797ca973dabdabb55a289636708
Evgeny Samoilov <zs.dp.ua@gmail.com>
2021-10-18 03:50:25 +0300

    реализована главная логика main logic implemented

--- TriangleArea/Program.cs
+++ TriangleArea/Program.cs
@@ -4,8 +4,20 @@ namespace TriangleArea
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int n = 6;
+            double[] elements = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                elements[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Vector vectors1 = new Vector(elements[0], elements[1], elements[2]);
+            Vector vectors2 = new Vector(elements[3], elements[4], elements[5]);
+            Triangle triangle = new Triangle(vectors1, vectors2);
+
+            Console.WriteLine(triangle.area);
+            Console.ReadKey();
         }
     }
 }
